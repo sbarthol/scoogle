392cf924a7e8ca0559ab52836ff418693bad19b7
Sacha Bartholme <sacha.bartholme@hotmail.com>
2020-12-16 18:48:07 +0100

    Logging configured

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,24 @@
+import akka.actor.{Actor, ActorSystem, PoisonPill, Props}
+import org.slf4j.LoggerFactory
+
+class Greeter extends Actor {
+
+  val logger = LoggerFactory.getLogger("oklm")
+  override def receive: Receive = { case s: String =>
+    logger.error(s)
+  }
+}
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello world")
+    val system = ActorSystem("pingpong")
+    val greeter = system.actorOf(props = Props[Greeter], name = "Sacha")
+    greeter ! (message = "coucou")
+    Thread.sleep(500)
+    val logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1")
+    logger.debug("Hello world.")
+    //greeter ! PoisonPill
+    system.terminate()
   }
 }
