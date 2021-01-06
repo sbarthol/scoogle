c1999b8b6a80ba7b8f6b02416d0daebbba24a76e
Sacha Bartholme <sacha.bartholme@hotmail.com>
2021-01-06 19:29:14 +0100

    CSS for pagination

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Pagination.css";
+import oBlack from "./images/o_blue.png";
+import oRed from "./images/o_red.png";
+import S from "./images/S.png";
+import c from "./images/c.png";
+import g from "./images/g.png";
+import l from "./images/l.png";
+import e from "./images/e.png";
 
 function PaginationButton({ onClick, pageNumber, selected }) {
   return (
-    <div>
-      <img alt="" src="o.png"></img>
-      <button onClick={onClick}>{pageNumber}</button>
+    <div className="OBox">
+      <img className="OImg" alt="" src={selected ? oBlack : oRed}></img>
+      <button
+        className={selected ? "OBtnSelected" : "OBtn"}
+        onClick={selected ? null : onClick}
+      >
+        {pageNumber}
+      </button>
     </div>
   );
 }
@@ -21,27 +33,44 @@ function Pagination({ getSearchResultsForPage, numberOfOs, selectedO }) {
     pages.push(firstPage + i);
 
   return (
-    <div>
-      {selectedO !== 1 && (
-        <button onClick={() => getSearchResultsForPage(selectedO - 1)}>
-          Previous
-        </button>
-      )}
-      {pages.map((page) => {
-        return (
-          <PaginationButton
-            key={page}
-            pageNumber={page}
-            selected={page === selectedO}
-            onClick={() => getSearchResultsForPage(page)}
-          />
-        );
-      })}
-      {numberOfOs !== selectedO && (
-        <button onClick={() => getSearchResultsForPage(selectedO + 1)}>
-          Next
-        </button>
-      )}
+    <div className="PaginationOuterBox">
+      <div className="PaginationInnerBox">
+        {selectedO === 1 ? (
+          <div className="PlaceHolderBtn"></div>
+        ) : (
+          <button
+            className="PreviousBtn"
+            onClick={() => getSearchResultsForPage(selectedO - 1)}
+          >
+            Previous
+          </button>
+        )}
+        <img className="LetterBox" src={S} alt=""></img>
+        <img className="LetterBox" src={c} alt=""></img>
+        {pages.map((page) => {
+          return (
+            <PaginationButton
+              key={page}
+              pageNumber={page}
+              selected={page === selectedO}
+              onClick={() => getSearchResultsForPage(page)}
+            />
+          );
+        })}
+        <img className="LetterBox" src={g} alt=""></img>
+        <img className="LetterBox" src={l} alt=""></img>
+        <img className="LetterBox" src={e} alt=""></img>
+        {selectedO === numberOfOs ? (
+          <div className="PlaceHolderBtn"></div>
+        ) : (
+          <button
+            className="NextBtn"
+            onClick={() => getSearchResultsForPage(selectedO + 1)}
+          >
+            Next
+          </button>
+        )}
+      </div>
     </div>
   );
 }
